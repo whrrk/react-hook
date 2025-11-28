b37d41789adad75c0071cec94ee26a557e6a7b3d
whrrk <jwh8712@naver.com>
2025-11-28 18:07:01 +0900

    init

---
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -0,0 +1,11 @@
+const Lesson1_1 = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
+      <p></p>
+    </div>
+  );
+};
+
+export default Lesson1_1;
---
+++ src/lessons/Lesson1/Lesson1_2.tsx
@@ -0,0 +1,26 @@
+const Lesson1_2 = () => {
+  return (
+    <div>
+      <div className="flex mb-5">
+        <label>
+          First Name:
+          <input type="text" className="border border-slate-500" />
+        </label>
+        <label>
+          Last Name:
+          <input type="text" className="border border-slate-500" />
+        </label>
+        <label>
+          Email:
+          <input type="text" className="border border-slate-500" />
+        </label>
+      </div>
+      <p>
+        <br />
+        <br />
+      </p>
+    </div>
+  );
+};
+
+export default Lesson1_2;
