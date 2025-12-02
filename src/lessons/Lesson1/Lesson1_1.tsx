d2d3b872c9ac7b65daa6a17126533ddee2250f30
whrrk <jwh8712@naver.com>
2025-12-02 10:09:16 +0900

    Lesson1

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,23 @@
+import { ChangeEvent, useState } from "react";
+
 const Lesson1_1 = () => {
+
+  const [age, setAge] = useState<number>(0);
+  const [name, setName] = useState<string>("ShinCode");
+
+  const handleClick = () => {
+    setAge((state) => state + 1);
+    setAge((state) => state + 1);
+    setAge((state) => state + 1);
+  }
+
   return (
     <div>
-      <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <input type="text" value={name} onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}/>
+      <button 
+      onClick={handleClick}
+      className="border p-2 rounded-md bg-red-100">Add Age</button>
+      <p>You are {age}</p>
     </div>
   );
 };
